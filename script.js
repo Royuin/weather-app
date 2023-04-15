1e43f692ff86ed82c6a7bb320beffb33bffe5ca9
Gage <77414756+Royuin@users.noreply.github.com>
2023-04-15 18:12:29 -0500

    fixed multiple searches added text to existing text and not replacing it. Also made displayWeatherData show weather condition and display icon for weather condition

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const tempNode = document.querySelector('.temp');
 const feelsLike = document.querySelector('.feels-like');
 const locationNode = document.querySelector('.location');
+const conditionNode = document.querySelector('.condition-text');
+const conditionIcon = document.querySelector('.condition-icon');
 
 function displayWeatherData(data) {
   tempNode.textContent = data.current.temp_c + '\u00B0C';
-  feelsLike.textContent += data.current.feelslike_c + '\u00B0C';
+  feelsLike.textContent = `Feels like ${data.current.feelslike_c} \u00B0C`;
 
-  locationNode.textContent +=
+  locationNode.textContent =
     `${data.location.name}, ` +
     `${data.location.region}, ` +
     `${data.location.country} `;
+  conditionNode.textContent = data.current.condition.text;
+  conditionIcon.src = data.current.condition.icon;
+  console.log(data.current.condition.icon);
 }
-const getWeatherData = async (location) => {
+
+async function getWeatherData(location) {
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=a2a56f72ff4949fabf611158231204&q=${location}&aqi=no`,
     {
@@ -20,12 +26,11 @@ const getWeatherData = async (location) => {
   ).then(function (response) {
     return response.json();
   });
+  console.log(response);
   displayWeatherData(response);
-};
+}
 
 getWeatherData('chicago');
-
-console.log(getWeatherData('montana'));
 
 const submitBtn = document.querySelector('.submit-button');
 
