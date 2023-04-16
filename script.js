d0827dc53d68df92a98bf88062922f1810520dd3
Gage <77414756+Royuin@users.noreply.github.com>
2023-04-16 14:30:32 -0500

    added measurement changing functionality to tempBtn

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const conditionIcon = document.querySelector('.condition-icon');
 const windSpeedNode = document.querySelector('.wind-speed');
 const humidityNode = document.querySelector('.humidity');
 const timeNode = document.querySelector('.time');
+const tempBtn = document.querySelector('.temp-button');
 
-function displayWeatherData(data) {
+function displayWeatherDataCelsius(data) {
   tempNode.textContent = data.current.temp_c + '\u00B0C';
   feelsLike.textContent = `Feels like ${data.current.feelslike_c} \u00B0C`;
 
@@ -26,7 +27,26 @@ function displayWeatherData(data) {
   timeNode.textContent = data.location.localtime;
 }
 
-async function getWeatherData(location) {
+function displayWeatherDataFahrenheit(data) {
+  tempNode.textContent = data.current.temp_f + '\u00B0F';
+  feelsLike.textContent = `Feels like ${data.current.feelslike_f} \u00B0F`;
+
+  locationNode.textContent =
+    `${data.location.name}, ` +
+    `${data.location.region}, ` +
+    `${data.location.country} `;
+
+  conditionNode.textContent = data.current.condition.text;
+  conditionIcon.src = data.current.condition.icon;
+
+  windSpeedNode.textContent = `Wind speed ${data.current.wind_mph} mph`;
+
+  humidityNode.textContent = `Humidity ${data.current.humidity}%`;
+
+  timeNode.textContent = data.location.localtime;
+}
+
+async function getWeatherData(location, measurement) {
   const response = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=3857d376c23d4bca9d400035231604&q=${location}&aqi=no`,
     {
@@ -35,11 +55,15 @@ async function getWeatherData(location) {
   ).then(function (response) {
     return response.json();
   });
-  console.log(response);
-  displayWeatherData(response);
+
+  if (measurement === 'C') {
+    displayWeatherDataCelsius(response);
+  } else if (measurement === 'F') {
+    displayWeatherDataFahrenheit(response);
+  }
 }
 
-getWeatherData('chicago');
+getWeatherData('chicago', 'F');
 
 const submitBtn = document.querySelector('.submit-button');
 
@@ -47,4 +71,16 @@ submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
   const inputValue = document.getElementById('location').value;
   getWeatherData(inputValue);
+});
+
+tempBtn.addEventListener('click', () => {
+  const measurement = tempBtn.textContent;
+  const location = locationNode.textContent;
+  if (measurement === 'C') {
+    getWeatherData(location, 'F');
+    tempBtn.textContent = 'F';
+  } else if (measurement === 'F') {
+    getWeatherData(location, 'C');
+    tempBtn.textContent = 'C';
+  }
 });
